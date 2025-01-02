2daa710398de0782feee2f98617e212e766e8453
Allie Jones <allie.jones@datadoghq.com>
2025-01-02 10:47:40 -0500

    Add template arguments to install script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot alliejones
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot alliejones --promptString email="allie.jones@datadoghq.com",usage="work"
